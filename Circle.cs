767a554623e99e3d2cb711fefbbc342862773047
samih <samih@LAPTOP-UHMUODJD>
2024-10-20 22:14:11 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5_OOP_SamiHarun_NET24
+{
+    public class Circle
+    {
+        
+            private double _radius;
+
+            // Konstruktor som tar radien som parameter
+            public Circle(double radius)
+            {
+                _radius = radius;
+            }
+
+            // Metod som beräknar arean av cirkeln
+            public double GetArea()
+            {
+                return _radius * _radius * Math.PI; // Beräknar arean (π * radie^2)
+            }
+        }
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LAB5_OOP_SamiHarun_NET24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapar ett objekt av klassen Circle med radie 5
+            Circle circle1 = new Circle(5);
+            // Skapar ett objekt av klassen Circle med radie 6
+            Circle circle2 = new Circle(6);
+
+            // Skriver ut arean för båda cirklarna
+            Console.WriteLine("Arean av cirkel med radie 5 är: " + circle1.GetArea());
+            Console.WriteLine("Arean av cirkel med radie 6 är: " + circle2.GetArea());
+
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
